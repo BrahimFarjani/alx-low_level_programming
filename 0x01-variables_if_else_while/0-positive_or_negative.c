befcb018a13fe91f102f6b5a943ba7764d638998
BrahimFarjani <farjaniibrahim2004@gmail.com>
2023-06-19 09:22:39 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,14 +9,14 @@
 int main(void)
 {
 	int n;
-	stand(time(0));
+	srand(time(0));
 	n=rand() - RAND_MAX / 2;
 	if (n>0)
 		printf("%d is positive\n", n);
 	else if (n==0)
 		printf("%d is zero\n", n);
 	else 
-		printf("Size of a long  int: %d byte(s)\n" , sizeof(long int));
+		printf("%d i negative \n", n);
 
         return(0);
 }
